31b86430270eeca4293f31f464d269300e9b4a97
luispadilla <luisangelpadilla004@gmail.com>
2025-02-19 16:54:54 -0500

    Actualización del error en el home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
         <h1 class="text-2xl md:text-3xl font-bold text-center lg:text-5xl text-gray-700">
             Welcome to <span class="text-yellow-500">&lt;Blog Coder&gt;</span> <span class="text-gray-900"> News</span>
         </h1>
-        <p class="text-gray-500 text-lg mt-1">Best Blog in the universe about Web develpment</p>
+        <p class="text-gray-500 text-lg mt-1">Best Blog in the universe about Web development</p>
         <a class="px-3 py-2 text-lg text-white bg-gray-800 rounded mt-5 inline-block"
             href="http://127.0.0.1:8000/blog">Start
             Reading</a>
